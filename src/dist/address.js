54444d7513b9eb41d31c8517ac27045b8f8d0d41
zyh <zhaoyuhua_2014@163.com>
2017-12-14 23:51:08 +0800

    调试地址列表接口和添加地址的接口

--- src/dist/address.js
+++ src/dist/address.js
@@ -13,6 +13,8 @@ define(function(require, exports, module){
 
     pub = {};
 
+
+	
     pub.module_id = $('[data-type]').attr('data-type');
     pub.logined = common.isLogin(); // 是否登录
 	console.log(common.secretKeyfn())
@@ -43,84 +45,7 @@ define(function(require, exports, module){
     pub.apiHandle = {
     	init : function(){
     		var me = this;
-    		me.address_manager.init();
-    	},
-    	address_manager : {
-    		init : function(){
-    			var me = this;
-    			common.ajaxPost($.extend({
-    				method:'user_address_show',
-    			}, pub.userBasicParam ),function( d ){
-    				d.statusCode == "100000" && me.apiData( d );
-    			});
-    		},
-    		apiData : function( d ){
-    			var 
-    				data = d.data,
-    			 	html = '';
-				for (var i in data) {
-					var obj = data[i];
-					html += '<div class="address_item">'
-					html += '	<div class="address_top true">'
-					html += '		<div class="address_name_phone clearfloat">'
-					html += '			<span class="float_left">耿亮</span>'
-					html += '			<span class="float_right">18315318515</span>'
-					html += '		</div>'
-					html += '		<p class="address_info">浙江省杭州市滨江区科技园文一西路45号文慧大厦1208室</p>'
-					html += '	</div>'
-					html += '	<div class="address_bottom clearfloat">'
-					html += '		<button class="float_left true">默认</button>'
-					html += '		<button class="float_right">编辑</button>'
-					html += '	</div>'
-					html += '</div>'
-					/*
-					html += '<div class="contain_address" addr-id="' + obj.id + '" >'
-					html += '	<div class="management_address"  >'
-					html += '        <div class="management_address_top clearfloat">'
-					html += '	         <div class="management_address_name">' + obj.consignee + '</div>'
-					html += '	         <div class="management_address_phone">' + obj.mobile + '</div>'
-					html += '        </div>'
-					html += '       <div class="management_address_bottom">' + obj.provinceName + obj.cityName + obj.countyName + obj.street + '</div>'
-				    html += '    </div>'
-					html += '	<div class="address_set clearfloat" >'
-					
-					html += '		<div class="default_address operate ' + ['','','default_bg'][ obj.isDefault+1 ] + '">默认地址</div>'
-
-					html += '		<div class="editor_address operate">编辑</div>'
-					html += '		<div class="delete_address operate">删除</div>'
-					html += '	</div>'
-					html += '</div>'*/
-				}
-				$(".address_box").append(html);
-				$.data( $('body')[0],'addressList', d.data );
-    		}
-    	},
-    	address_default : {
-    		init : function(){
-    			common.ajaxPost($.extend({
-    				method:'user_address_default',
-    				addressId : pub.addrId
-    			},pub.userBasicParam),function( d ){
-    				if ( d.statusCode == "100000" ) {
-						$(".default_bg",".address_management").removeClass("default_bg");
-						pub.defaultBtn.addClass("default_bg");
-					} else{
-						common.prompt( d.statusStr )
-					}
-    			});
-    		}
-    	},
-    	address_delete : {
-    		init : function(){
-    			common.ajaxPost($.extend({
-    				method : 'user_address_delete',
-    				addrId : pub.addrId,	
-    			},pub.userBasicParam),function( d ){
-    				if( d.statusCode == "100000" ){
-                    	
-                    }
-    			});
-    		}
+    		pub.apiHandle.address_manager.init();
     	},
     	trueFn:function(){
 			var arr = $.data($("body")[0],"addressList");
@@ -130,75 +55,220 @@ define(function(require, exports, module){
 			pub.apiHandle.address_delete.init();  // 删除
     	}
     };
-
-    // 事件处理 
     pub.eventHandle = {
-    	init : function(){
-    		// 选择默认地址
-    		$(".address_management").on('click',".operate",function(){
-				var 
-    			$this = $(this),
-    			isEditor = $this.is('.editor_address'),
-    			isDelete = $this.is('.delete_address'),
-    			isDefault = $this.is('.default_address'),
-    			isCur = $this.is('.default_bg');
-
-    			pub.addrId = $this.parents('.contain_address').attr('addr-id');
-    			// 默认地址选择
-    			if( isDefault && !isCur ){ 
-    				pub.defaultBtn = $this;
-    				pub.apiHandle.address_default.init(); 
-    				return;
-    			} 
-    			// 删除
-    			if( isDelete ){  
-    				pub.index = $this.parents('.contain_address').index();
-    				var data = {
-						type:1,
-						title:'确认删除?',
-						canclefn:'cancleFn',
-						truefn:'trueFn'
+    	init : function (){
+    		$(".header_left").on("click",function(){
+    			window.history.back();
+    		})
+    	}
+    }
+    //地址列表接口及事件
+    pub.address_manager = {
+    	//地址设为默认数据数组
+    	add_deffer :["设为默认","","默认"],
+		//地址列表
+		init : function(){
+			var me = this;
+			common.ajaxPost($.extend({
+				method:'user_address_show',
+			}, pub.userBasicParam ),function( d ){
+				d.statusCode == "100000" && me.apiData( d );
+			});
+		},
+		//地址列表解析
+		apiData : function( d ){
+			var 
+				data = d.data,
+			 	html = '';
+			if (data.length == 0) {
+				$(".address_box").html("暂无地址信息！").css({"font-size":"32px","text-align":"center"})
+			}
+			for (var i in data) {
+				var obj = data[i];
+				html += '<div class="address_item '+(obj.isDefault == 1 ? "actived": "")+'" addr-id="' + obj.id + '">'
+				html += '	<div class="address_top">'
+				html += '		<div class="address_name_phone clearfloat">'
+				html += '			<span class="float_left">' + obj.receiverName + '</span>'
+				html += '			<span class="float_right">' + obj.receiverName + '</span>'
+				html += '		</div>'
+				html += '		<p class="address_info">' + obj.receiverName + '</p>'
+				html += '	</div>'
+				html += '	<div class="address_bottom clearfloat">'
+				html += '		<button class="float_left operate">' + pub.address_manager.add_deffer[obj.isDefault + 1] + '</button>'
+				html += '		<button class="float_right operate">编辑</button>'
+				html += '	</div>'
+				html += '</div>'
+			}
+			$(".address_box").append(html);
+			$.data( $('body')[0],'addressList', d.data );
+		},
+		//设为默认地址
+		address_default : {
+    		init : function(){
+    			common.ajaxPost($.extend({
+    				method:'user_address_default',
+    				addressId : pub.address.addressId
+    			},pub.userBasicParam),function( d ){
+    				if ( d.statusCode == "100000" ) {
+						
+					} else{
+						common.prompt( d.statusStr )
 					}
-					common.alertMaskApp(JSON.stringify(data));
-					
-    			}
-
-    			if( isEditor ){
-    				var 
-    				index = $this.parents('.contain_address').index(),
-    				addrInfo = common.JSONStr( $.data($('body')[0],'addressList')[index] );
-
-					common.addressData.setItem( addrInfo );
-					common.jumpLinkPlainApp( "地址列表",!pub.searchAddr ? 'html/address.html' : 'html/address.html?addr=' + pub.searchAddr );
-					
-    			}
-			});
-			$(".add_address").on("click",function(){
-				common.addressData.removeItem();
-                common.jumpLinkPlainApp( "地址列表",!pub.searchAddr ? 'html/address.html' : 'html/address.html?addr=' + pub.searchAddr );
-			})
-
-			
-
-    		$(".address_management").on('click',".management_address",function(){
-    			var 
-    			index = $(this).parents('.contain_address').index(),
-    			addrInfo = common.JSONStr( $.data($('body')[0],'addressList')[index] ); // 取出数据并存储
-				common.addressData.setItem( addrInfo );
-                common.goBackApp(1,true,'html/order_set_charge.html')
-			});
-    		!common.addType.getItem() && $('.address_management').off('click','.management_address'); // 判断是否从订单进入
+    			});
+    		}
     	},
-    };
-
-
+    	//事件处理
+    	eventHandle : {
+    		init : function (){
+    			//新增地址
+    			$(".header_right.right_text").on("click",function(){
+    				window.location.href = "../html/my_address.html"
+    			})
+    			// 选择默认地址
+	    		$(".address_box").on('click',".operate",function(){
+					var 
+	    			$this = $(this),
+	    			isEditor = $this.is('.float_right'),
+	    			isDelete = $this.is('.delete_address'),
+	    			isDefault = $this.is('.float_left'),
+	    			isCur = $this.is('.default_bg');
+	
+					pub.address.nood = $this.parents('.address_item');
+					
+	    			pub.addressId = pub.address.nood.attr('addr-id');
+	    			// 默认地址选择
+	    			if( isDefault && !isCur ){ 
+	    				pub.address_manager.address_default.init(); 
+	    				return;
+	    			} 
+	    			// 删除
+	    			if( isDelete ){  
+	    				pub.index = $this.parents('.contain_address').index();
+	    				var data = {
+							type:1,
+							title:'确认删除?',
+							canclefn:'cancleFn',
+							truefn:'trueFn'
+						}
+						common.alertMaskApp(JSON.stringify(data));
+						
+	    			}
+					//编辑
+	    			if( isEditor ){
+	    				var 
+	    				index = pub.address.nood.index(),
+	    				addrInfo = common.JSONStr( $.data($('body')[0],'addressList')[index] );
+	
+						common.addressData.setItem( addrInfo );
+						//common.jumpLinkPlainApp( "地址列表",!pub.searchAddr ? 'html/address.html' : 'html/address.html?addr=' + pub.searchAddr );
+						window.location.href = "../html/my_address.html?addressId="+pub.addressId
+	    			}
+				});
+				$(".add_address").on("click",function(){
+					common.addressData.removeItem();
+	                common.jumpLinkPlainApp( "地址列表",!pub.searchAddr ? 'html/address.html' : 'html/address.html?addr=' + pub.searchAddr );
+				})
+	
+				
+	
+	    		$(".address_management").on('click',".management_address",function(){
+	    			var 
+	    			index = $(this).parents('.contain_address').index(),
+	    			addrInfo = common.JSONStr( $.data($('body')[0],'addressList')[index] ); // 取出数据并存储
+					common.addressData.setItem( addrInfo );
+	                common.goBackApp(1,true,'html/order_set_charge.html')
+				});
+	    		!common.addType.getItem() && $('.address_management').off('click','.management_address'); // 判断是否从订单进入
+    		}
+    	}
+    }
+    //地址详情
+    pub.address = {
+    	beforeInit:function(){
+    		pub.address.data = JSON.parse(common.addressData.getItem());
+    		var noods = $(".list_item");
+    		noods.eq(0).find(".float_right input").val(pub.address.data.receiverName)
+    		noods.eq(1).find(".float_right input").val(pub.address.data.receiverMobile)
+    		noods.eq(2).find(".float_right input").val(pub.address.data.countyId)
+    		noods.eq(3).find(".float_right input").val(pub.address.data.address)
+    	},
+    	init : function (){
+    		if(common.getUrlParam("addressId")){
+    			pub.address.addressId = common.getUrlParam("addressId");
+    			console.log(pub.address.addressId)
+    			pub.address.beforeInit();
+    		}else{
+    			pub.address.addressId = null;
+    		}
+    	},
+    	address_update : {
+    		init : function(){
+    			common.ajaxPost($.extend({
+    				method:'user_address_update',
+    				receiverName:pub.address.receiverName,
+    				receiverMobile:pub.address.receiverMobile,
+    				provinceId:pub.address.provinceId,
+    				cityId:pub.address.cityId,
+    				countyId:pub.address.countyId,
+    				//areaName:pub.address.areaName,//社区名称
+    				address:pub.address.address,//联系地址
+    				addressId : pub.address.addressId,//地址ID新增则为空
+    			},pub.userBasicParam),function( d ){
+    				if ( d.statusCode == "100000" ) {
+						//$(".default_bg",".address_management").removeClass("default_bg");
+						//pub.defaultBtn.addClass("default_bg");
+					} else{
+						common.prompt( d.statusStr )
+					}
+    			});
+    		}
+    	},
+    	//地址删除
+    	address_delete : {
+    		init : function(){
+    			common.ajaxPost($.extend({
+    				method : 'user_address_delete',
+    				addressId : pub.address.addressId,	
+    			},pub.userBasicParam),function( d ){
+    				if( d.statusCode == "100000" ){
+                    	
+                    } else{
+						common.prompt( d.statusStr )
+					}
+    			});
+    		}
+    	},
+    	//地址详情事件管理
+    	eventHandle : {
+			init: function(){
+				$(".header_right.right_text").on("click",function(){
+					//pub.address. = {
+						pub.address.receiverName="赵玉华1";
+						pub.address.receiverMobile="14700080905";
+						pub.address.provinceId="33";
+						pub.address.cityId='3301';
+						pub.address.countyId='330109';
+						pub.address.areaName="华业大厦"//社区名称
+						pub.address.address="2302"//联系地址
+						pub.address.addressId = null//地址ID新增则为空
+					//}
+					pub.address.address_update.init();
+				})
+			}
+		}
+    	
+    }
 
     // 模块初始化
     pub.init = function(){
     	if (pub.module_id == "address_list") {
-    		pub.apiHandle.init();
-			pub.eventHandle.init();
+    		 pub.address_manager.init()
+			pub.address_manager.eventHandle.init();
+    	}else if (pub.module_id == "address"){
+    		pub.address.init();
+    		pub.address.eventHandle.init()
     	}
+    	pub.eventHandle.init()
     };
     
     module.exports = pub;
