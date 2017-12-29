047c4425dd7c7dab5e1f0f6ef55b7759d14bad92
zhaoyuhua <zhaoyuhua_2014@163.com>
2017-12-29 15:40:21 +0800

    无从下手，下次再写吧

---
+++ src/module/banner.js
@@ -0,0 +1,15 @@
+define(function(require, exports, module) {
+	
+	require("swiper");
+	require("swiperCSS");
+	
+  // 模块代码
+  module.exports = banner;
+  
+  
+  banner={};
+  banner.options = {
+  	
+  }
+
+})
